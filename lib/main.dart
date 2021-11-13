02950be5470b0e2b60c2ba4355ac0b6c4cc8742a
henryadebayo <henryadebayo250@gmail.com>
2021-11-13 03:30:25 +0100

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:animation_voostech/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
